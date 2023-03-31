917c4ee6286f19200bc151c65af52046942d1693
Andrei Kernazhytski <108015888+Kernazhytski@users.noreply.github.com>
2023-03-31 10:07:00 +0300

    Сделал функцию получения списка и добавления для техники

--- server/src/main/java/com/example/server/controllers/VehicleController.java
+++ server/src/main/java/com/example/server/controllers/VehicleController.java
@@ -1,12 +1,28 @@
 package com.example.server.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.server.models.Vehicle;
+import com.example.server.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/vehicle")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class VehicleController {
 
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@RequestBody Vehicle vehicle) {
+        vehicleService.addVehicle(vehicle);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> getListAll(@RequestParam(value = "type",defaultValue = "all") String type) {
+        return new ResponseEntity<>(vehicleService.getList(type),HttpStatus.OK);
+    }
 }
--- server/src/main/java/com/example/server/services/VehicleService.java
+++ server/src/main/java/com/example/server/services/VehicleService.java
@@ -2,8 +2,15 @@ package com.example.server.services;
 
 import com.example.server.models.Vehicle;
 import com.example.server.repo.VehicleRepo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class VehicleService {
@@ -11,7 +18,40 @@ public class VehicleService {
     @Autowired
     private VehicleRepo vehicleRepo;
 
-    public void addVehicle(Vehicle vehicle){
+    @Autowired
+    private EntityManager entityManager;
+
+    CriteriaBuilder criteriaBuilder;
+    CriteriaQuery<Vehicle> criteriaQuery;
+    Root<Vehicle> root;
+    Predicate predicate;
+
+    public void addVehicle(Vehicle vehicle) {
         vehicleRepo.save(vehicle);
+    }
+
+    public List<Vehicle> getList(String type) {
+        if (type.equals("all")) {
+            return vehicleRepo.findAll();
+        } else {
+            criteriaBuilder = entityManager.getCriteriaBuilder();
+
+            // Создаем объект CriteriaQuery, который будет использоваться для построения запроса
+            criteriaQuery = criteriaBuilder.createQuery(Vehicle.class);
+
+            // Создаем корневой объект, который представляет тип данных, по которым мы хотим выполнить запрос
+            root = criteriaQuery.from(Vehicle.class);
+            // Создаем условие для выборки записей
+            // В данном случае мы выбираем записи, у которых поле "name" равно "John" и поле "email" равно "john@example.com"
+            predicate = criteriaBuilder.and(
+                    criteriaBuilder.equal(root.get("type"), type)
+            );
+
+            // Добавляем условие в запрос
+            criteriaQuery.where(predicate);
+
+            // Выполняем запрос и получаем результат
+            return entityManager.createQuery(criteriaQuery).getResultList();
+        }
     }
 }
